0ee8e05d8c7ac21f76cd7602e61b9187ddf9f318
room141 <vbzala141@gmail.com>
2025-11-17 17:54:18 +0200

    База данни

---
+++ Migrations/20251117154539_SeedRoles.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicAndMind2.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+    table: "AspNetRoles",
+    columns: new[] { "Id", "Name", "NormalizedName" },
+    values: new object[,]
+    {
+        { Guid.NewGuid().ToString(), "Admin", "ADMIN" },
+        { Guid.NewGuid().ToString(), "Moderator", "MODERATOR" },
+        { Guid.NewGuid().ToString(), "Customer", "CUSTOMER" }
+    }
+);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
